a1f6ec4be8829a39da61e7519b93455f42f770b5
DaniilZ77 <zolindaniil7705@gmail.com>
2025-02-23 23:47:48 +0300

    fix: refactored

--- user/add1.c
+++ user/add1.c
@@ -1,52 +1,35 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
 #define BUF_LEN 50
-
 int main(int argc, char *argv[]) {
     char buf[BUF_LEN];
-
-    char err_space[] = "Read failure: must contain one space\n";
-    int cc, i, sp = 0, err_space_len = strlen(err_space);
+    int cc, i, sp = 0;
     char c;
+    char *cur;
     for (i = 0; i + 1 < BUF_LEN; ++i) {
         cc = read(0, &c, 1);
-        if (cc < 1) {
-            break;
-        }
+        if (cc < 1) break;
         buf[i] = c;
-
-        if (c == '\n') {
-            break;
-        }
+        if (c == '\n') break;
         if (!('0' <= c && c <= '9') && c != ' ') {
             write(2, "Read failure: must contain digits\n", 35);
             exit(1);
         }
-        if (c == ' ' && sp++ != 0) {
-            write(2, err_space, err_space_len);
-            exit(1);
-        }
+        if (c == ' ' && (cur = buf + i) && ++sp);
         if (i + 2 == BUF_LEN) {
             write(2, "Read failure: buffer overflow\n", 31);
             exit(1);
         }
     }
-
-    if (sp == 0) {
-        write(2, err_space, err_space_len);
+    if (sp != 1) {
+        write(2, "Read failure: must contain one space\n", 38);
         exit(1);
     }
-
     buf[i] = '\0';
-
-    char* cur = buf;
-    int n1 = atoi(cur);
-    while (*cur != ' ') ++cur;
-    ++cur;
-    int n2 = atoi(cur);
-
+    int n1 = atoi(buf);
+    int n2 = atoi(++cur);
     printf("|%s|\n", buf);
     printf("%d\n", n1 + n2);
+    exit(0);
 }
--- user/add3.c
+++ user/add3.c
@@ -4,14 +4,14 @@
 
 int main(int argc, char *argv[]) {
    int res = add(5, 10);
-   printf(res == 15 ? "Test 1: success\n" : "Test 1: error\n");
+   printf(res == 15 ? "Test 1: success\n" : "Test 1: failed\n");
 
    res = add(123, 321);
-   printf(res == 444 ? "Test 2: success\n" : "Test 2: error\n");
+   printf(res == 444 ? "Test 2: success\n" : "Test 2: failed\n");
 
    res = add(-5, 10);
-   printf(res == 5 ? "Test 3: success\n" : "Test 3: error\n");
+   printf(res == 5 ? "Test 3: success\n" : "Test 3: failed\n");
 
    res = add(5, -10);
-   printf(res == -5 ? "Test 4: success\n" : "Test 4: error\n");
+   printf(res == -5 ? "Test 4: success\n" : "Test 4: failed\n");
 }
