dcaec13275e6948eb4b95ec9064cdd62e83b391f
DaniilZ77 <zolindaniil7705@gmail.com>
2025-03-16 14:32:40 +0300

    fix: dynamic allocation

--- user/ps.c
+++ user/ps.c
@@ -3,17 +3,26 @@
 #include "user/user.h"
 #include "procinfo.h"
 
-#define LIM 10
+#define MAX_TRIES 15
 
 int main(int argc, char *argv[]) {
-    struct procinfo plist[LIM];
-    int ret = ps_listinfo(plist, LIM);
-    if (ret == -1) {
-        fprintf(2, "error: must increase buffer size\n");
-        exit(1);
-    } else if (ret == -2) {
-        fprintf(2, "error: failed to write into provided address\n");
-        exit(1);
+    int size = 1;
+    struct procinfo* plist = (struct procinfo*)malloc(sizeof(struct procinfo) * size);
+    int ret;
+    int tries = 0;
+    while (tries < MAX_TRIES) {
+        ret = ps_listinfo(plist, size);
+        if (ret == -1) {
+            size *= 2;
+            free(plist);
+            plist = (struct procinfo*)malloc(sizeof(struct procinfo) * size);
+            ++tries;
+            continue;
+        } else if (ret == -2) {
+            fprintf(2, "error: failed to write into provided address\n");
+            exit(1);
+        }
+        break;
     }
 
     printf("total procs: %d\n---\n", ret);
@@ -23,8 +32,7 @@ int main(int argc, char *argv[]) {
         printf("ppid: %d\n", plist[i].parent_pid);
         printf("name: %s\n", plist[i].name);
         printf("state: ");
-        switch (plist[i].state)
-        {
+        switch (plist[i].state) {
         case procinfostate_used:
             printf("used");
             break;
@@ -42,6 +50,7 @@ int main(int argc, char *argv[]) {
         }
         printf("\n---\n");
     }
+    free(plist);
 
     exit(0);
 }
