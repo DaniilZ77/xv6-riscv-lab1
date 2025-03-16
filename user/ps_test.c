90a3098dfe37d66cb58fe58c5a7c50230a1150ec
DaniilZ77 <zolindaniil7705@gmail.com>
2025-03-16 14:55:21 +0300

    fix: added tests

--- user/ps_test.c
+++ user/ps_test.c
@@ -4,10 +4,20 @@
 #include "procinfo.h"
 
 int main(int argc, char *argv[]) {
-    struct procinfo plist[1];
-    int ret = ps_listinfo(plist, 1);
+    struct procinfo a[1];
+    int ret = ps_listinfo(a, 1);
     printf(ret == -1 ? "Test1(insufficient lim): passed\n" : "Test1(insufficient lim): failed\n");
 
     ret = ps_listinfo((struct procinfo *)123, 3);
     printf(ret == -2 ? "Test2(incorrect address): passed\n" : "Test2(incorrect address): failed\n");
+
+    struct procinfo b[32];
+    ret = ps_listinfo(b, 32);
+    printf(ret == 3 ? "Test3(success): passed\n" : "Test3(success): failed\n");
+
+    ret = ps_listinfo(0, 32);
+    printf(ret == 3 ? "Test4(success): passed\n" : "Test4(success): failed\n");
+
+    ret = ps_listinfo(0, 1);
+    printf(ret == -1 ? "Test5(insufficient lim): passed\n" : "Test5(insufficient lim): failed\n");
 }
